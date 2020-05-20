617c84c8945c74e3a8befd439aeadb0a300f5379
Paul McIntosh <ptmci1@student.monash.edu>
2020-05-20 23:45:36 +1000

    Added basic ZombieArm and ZombieLeg

---
+++ src/game/ZombieArm.java
@@ -0,0 +1,11 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieArm extends WeaponItem {
+	//TODO implement ZombieLimbWeapon enum capability, and add javadoc comments
+
+	public ZombieArm() {
+		super("Zombie Arm", '-', 15, "whacks");
+	}
+}
---
+++ src/game/ZombieLeg.java
@@ -0,0 +1,13 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieLeg extends WeaponItem {
+	//TODO implement ZombieLimbWeapon enum capability, and add javadoc comments
+
+	public ZombieLeg() {
+		super("Zombie Leg", '|', 20, "whacks");
+		// TODO Auto-generated constructor stub
+	}
+
+}
