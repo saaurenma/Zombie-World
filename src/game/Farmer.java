64a297d7959582ced368d9501b8d69c186eef443
Saauren Mankad <sman0039@student.monash.edu>
2020-05-20 19:18:45 +1000

    added farmer class

---
+++ src/game/Farmer.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Farmer extends Human {
+
+	protected Farmer(String name, char displayChar, int hitPoints) {
+		super(name, displayChar, hitPoints);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+
+}
