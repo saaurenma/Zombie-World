4d8d95c5f9711c53af31536a50fff633f352f414
Saauren Mankad <sman0039@student.monash.edu>
2020-05-22 07:00:50 +1000

    Implement the passage of time for Corpse and Crop

--- src/game/Corpse.java
+++ src/game/Corpse.java
@@ -1,12 +1,24 @@
 package game;
 
+import edu.monash.fit2099.engine.Location;
+
 public class Corpse extends PortableItem {
+	
+	private int age = 0;
 
 	public Corpse(String name, char displayChar) {
 		super(name, displayChar);
 		// TODO Auto-generated constructor stub
 	}
 	
+	@Override
+	public void tick(Location currentLocation) {
+		super.tick(currentLocation);
+		
+		// increase day by 1
+		age++;
+
+	}
 	
 
 }
--- src/game/Crop.java
+++ src/game/Crop.java
@@ -1,14 +1,22 @@
 package game;
 
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
 
 public class Crop extends Ground{
+	private int age = 0;
 
-	public Crop(char displayChar) {
-		super(displayChar);
+	public Crop(CropRipeUnripe isRipe) {
+		super('C');
 		// TODO Auto-generated constructor stub
 	}
 	
-	
+	@Override
+	public void tick(Location location) {
+		
+		super.tick(location);
+		age++;
+		
+	}
 
 }
