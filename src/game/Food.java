5a2974a37726fd4f90027caf1812d7a928f95b7d
Saauren Mankad <sman0039@student.monash.edu>
2020-05-21 16:56:03 +1000

    Add food class with auto generated methods

---
+++ src/game/Food.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Food extends PortableItem {
+
+	public Food(String name, char displayChar) {
+		super(name, displayChar);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
