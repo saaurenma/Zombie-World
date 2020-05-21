d153f031bd23dc9127ac33d8137d0350e5c11195
Saauren Mankad <sman0039@student.monash.edu>
2020-05-22 09:03:20 +1000

    Add rise from dead attribute to Corpse

--- src/game/Corpse.java
+++ src/game/Corpse.java
@@ -1,14 +1,27 @@
 package game;
 
+import java.util.Random;
+
 import edu.monash.fit2099.engine.Location;
 
 public class Corpse extends PortableItem {
 	
 	private int age = 0;
+	private int timeToRiseFromDead;
+	
 
 	public Corpse(String name, char displayChar) {
 		super(name, displayChar);
-		// TODO Auto-generated constructor stub
+		this.timeToRiseFromDead = getRandomInteger();
+	}
+	
+	private int getRandomInteger(){
+		
+		Random rand = new Random();
+		int randNumber;
+		randNumber = Math.abs(rand.nextInt(5) + 1) + 5;
+		
+		return randNumber;
 	}
 	
 	@Override
