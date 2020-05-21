e438797b2153d71007c88bd5cc4a44fdbef625b6
Saauren Mankad <sman0039@student.monash.edu>
2020-05-21 16:31:39 +1000

    Add Corpse class with auto generated constructor

---
+++ src/game/Corpse.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Corpse extends PortableItem {
+
+	public Corpse(String name, char displayChar) {
+		super(name, displayChar);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
