70ebbfa2745f8e608abbb734cf83b92016f944f7
Saauren Mankad <sman0039@student.monash.edu>
2020-05-22 07:07:33 +1000

    Add behaviour array for Farmer and create SowBehaviour and FertilizeBehaviour

--- src/game/Farmer.java
+++ src/game/Farmer.java
@@ -1,7 +1,13 @@
 package game;
 
 public class Farmer extends Human {
-
+	
+	private Behaviour[] behaviours = {
+			new SowBehaviour(),
+			new FertilizeBehaviour(),
+			new WanderBehaviour()
+	};
+	
 	protected Farmer(int hitPoints) {
 		super("farmer", 'F', hitPoints);
 		// TODO Auto-generated constructor stub
---
+++ src/game/SowBehaviour.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.util.Random;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class SowBehaviour implements Behaviour {
+	Random rand = new Random();
+
+	@Override
+	public Action getAction(Actor actor, GameMap map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
