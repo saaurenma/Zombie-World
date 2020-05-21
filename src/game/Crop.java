e48de1bfeaee411ae1b57b557120856e986a5a43
Saauren Mankad <sman0039@student.monash.edu>
2020-05-21 16:32:37 +1000

    Add Crop class with auto generated constructor

---
+++ src/game/Crop.java
@@ -0,0 +1,14 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+public class Crop extends Ground{
+
+	public Crop(char displayChar) {
+		super(displayChar);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
