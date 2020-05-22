7c5c8ab90b6332ef99b106a764d2028426cea28a
Saauren Mankad <sman0039@student.monash.edu>
2020-05-22 17:21:09 +1000

    Completed Implementation of Sow feature

--- src/game/SowAction.java
+++ src/game/SowAction.java
@@ -3,19 +3,30 @@ package game;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
 
 public class SowAction extends Action {
+	
+	Location sowLocation;
 
+	
+	public SowAction(Location location) {
+		this.sowLocation = location;
+	}
+	
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		sowLocation.addItem(new Crop(CropRipeUnripe.UNRIPE));
+		String executeDescription = actor + " sows a crop at location " + "("+sowLocation.x() + "," + sowLocation.y()+")";
+		return executeDescription;
 	}
 
 	@Override
 	public String menuDescription(Actor actor) {
-		// TODO Auto-generated method stub
-		return null;
+		String executeDescription = actor + " sows a crop";
+		return executeDescription;
+
 	}
 
 }
--- src/game/SowBehaviour.java
+++ src/game/SowBehaviour.java
@@ -1,19 +1,46 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Exit;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
 
 public class SowBehaviour implements Behaviour {
 	Random rand = new Random();
 
+
+	
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
-		// TODO Auto-generated method stub
+		
+		
+		List<Exit> exits = new ArrayList<Exit>(map.locationOf(actor).getExits());
+		Collections.shuffle(exits);
+		int randomNum = Math.abs(rand.nextInt() % 3) + 1;
+		
+		
+		for (Exit e: exits) {
+			Ground currentGround = e.getDestination().getGround();
+			if (!(currentGround instanceof Dirt))
+				continue;
+			else if (randomNum == 1) {
+				return new SowAction(e.getDestination());
+			}
+		}
 		return null;
 	}
+		
+	
+	
+	
 
 
 }
