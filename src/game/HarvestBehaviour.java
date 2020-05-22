91b7bf44e7f8f5b4352391b35dfb1bbee9704da6
Saauren Mankad <sman0039@student.monash.edu>
2020-05-22 17:40:09 +1000

    Add HarvestBehaviour class with auto generated methods

---
+++ src/game/HarvestBehaviour.java
@@ -0,0 +1,16 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class HarvestBehaviour implements Behaviour {
+
+	
+	@Override
+	public Action getAction(Actor actor, GameMap map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
