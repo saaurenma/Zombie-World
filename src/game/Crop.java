c956deaf429570e01f8e049d9e28b5ad6e8581c2
Saauren Mankad <sman0039@student.monash.edu>
2020-05-22 12:41:43 +1000

    Implement Fertilize method

--- src/game/Crop.java
+++ src/game/Crop.java
@@ -1,22 +1,36 @@
 package game;
 
-import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.Location;
 
-public class Crop extends Ground{
-	private int age = 0;
+public class Crop extends Item{
+	private int age;
+	private int ripenTime;
 
 	public Crop(CropRipeUnripe isRipe) {
-		super('C');
-		// TODO Auto-generated constructor stub
+		super("crop", 'C', false);
+		this.addCapability(isRipe);
+		this.age = 0;
+		this.ripenTime = 20;
+	}
+	
+	public void fertilize() {
+		
+		if (ripenTime > 0) {
+			ripenTime -= 10;
+		}
+		
+		else {
+			this.removeCapability(CropRipeUnripe.UNRIPE);
+			this.addCapability(CropRipeUnripe.RIPE);
+		}
 	}
 	
 	@Override
 	public void tick(Location location) {
-		
 		super.tick(location);
-		age++;
-		
+		this.age++;
+		this.ripenTime--;
 	}
 
 }
