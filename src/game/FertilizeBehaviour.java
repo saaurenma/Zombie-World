2d9bb87cace84be734615a8e03bf15e0bd002c82
Saauren Mankad <sman0039@student.monash.edu>
2020-05-22 12:42:28 +1000

    Complete FertlizeBehaviour

--- src/game/FertilizeBehaviour.java
+++ src/game/FertilizeBehaviour.java
@@ -1,15 +1,30 @@
 package game;
 
+import java.util.List;
+
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
 
 public class FertilizeBehaviour implements Behaviour {
 
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Location locationOfActor = map.locationOf(actor);
+		List<Item> items = locationOfActor.getItems();
+		
+		for(int i = 0; i < items.size(); i++) {
+			Item currentItem = items.get(i);
+			if(currentItem.toString().equals("crop")) {
+				if (currentItem.hasCapability(CropRipeUnripe.UNRIPE)){
+					return new FertilizeAction(currentItem);
+				}			
+			}
+		}
+		return null;		
 	}
 
 }
