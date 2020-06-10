366a9cfcc9c97e7091227161a654882be54bf2f1
Saauren Mankad <sman0039@student.monash.edu>
2020-06-11 08:27:20 +1000

    Implement ChantAction description in menu and when the executed string

--- src/game/ChantAction.java
+++ src/game/ChantAction.java
@@ -1,22 +1,35 @@
 package game;
 
+import java.util.ArrayList;
+
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
 
 public class ChantAction extends Action{
+	
+	ArrayList<Location> spawnLocations;
+	
+	public ChantAction(ArrayList<Location> zombieSpawnLocations) {
+		
+		this.spawnLocations = zombieSpawnLocations;
+		
+	}
 
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// things here
+		
+		String result = actor + " chanted, 5 zombies spawned!";
+		return result;
 	}
 
 	@Override
 	public String menuDescription(Actor actor) {
 		// do not hard code Mambo Marie actor as we may want to
 		// give this action to another boss in the future for example
-		
 		String description = actor + " chants";
 		
 		return description;
