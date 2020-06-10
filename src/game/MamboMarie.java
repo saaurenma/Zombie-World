fde3946743626d07bc7383f23b6ff6444dca9e52
Saauren Mankad <sman0039@student.monash.edu>
2020-06-10 19:45:35 +1000

    Added Mambo Marie class

---
+++ src/game/MamboMarie.java
@@ -0,0 +1,39 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actions;
+import edu.monash.fit2099.engine.Display;
+import edu.monash.fit2099.engine.DoNothingAction;
+import edu.monash.fit2099.engine.GameMap;
+
+public class MamboMarie extends ZombieActor{
+	
+	// use an array to store WanderBehaviour, since we may want to add more
+	// in the future. Also ensures consistency with the other ZombieActor child
+	// classes
+	private Behaviour[] behaviours = {new WanderBehaviour(), new ChantBehaviour()};
+	
+	/**
+	 * Constructor for MamboMarie, starts with 30 more hitpoints than a
+	 * normal zombie
+	 */
+	public MamboMarie() {
+		super("Mambo Marie", 'M', 130, ZombieCapability.UNDEAD);
+		
+	}
+	
+	
+
+	@Override
+	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+		for (Behaviour behaviour : behaviours) {
+			Action action = behaviour.getAction(this, map);
+			if (action != null)
+				return action;
+		}
+		return new DoNothingAction();
+	}
+	
+	
+
+}
